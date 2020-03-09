41ec023a7df950318c14275b2e7ba59e5dbaff3e
James E. Bell Jr <github@travelerbell.net>
2020-03-09 07:40:57 -0500

    feat: Service operations complete, need to add state history
    Signed-off-by: James E. Bell Jr <github@travelerbell.net>

--- dashboard/service_view.py
+++ dashboard/service_view.py
@@ -1,14 +1,26 @@
 from django.views.generic import DetailView
-from django.views.generic.edit import UpdateView
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, reverse
+from django.views.generic.edit import CreateView, UpdateView
+from django.shortcuts import reverse
 from .models import Service
+
+
+class ServiceNewView(CreateView):
+    model = Service
+    fields = ['name', 'description', 'public', 'enabled', 'is_capability']
+    template_name_suffix = "_create_form"
+    initial = {
+        'enabled': True,
+        'public': False,
+    }
+
+    def get_success_url(self):
+        return reverse('dash:service_view', kwargs={'pk': self.object.id})
 
 
 class ServiceEditView(UpdateView):
     model = Service
     template_name_suffix = "_update_form"
-    fields = ['name', 'description', 'public', 'enabled']
+    fields = ['name', 'description', 'public', 'enabled', 'is_capability']
 
     def get_success_url(self):
         return reverse('dash:service_view', kwargs={'pk': self.object.id})
@@ -18,10 +30,9 @@ class ServiceView(DetailView):
     """View of a service"""
     template_name = "dashboard/service.html"
     model = Service
-    initial = {}
+    initial = {'enabled': True, 'public': False}
 
-    def post(self, req: HttpRequest, *args, **kwargs):
-        form = self.form_class(req.POST)
-        if form.is_valid():
-            return redirect(Service.objects.get(id=form.id))
-        return HttpResponse("hello", status=401)
+    def get_context_data(self, **kwargs):
+        context = self.initial
+        context.update(super().get_context_data(**kwargs))
+        return context
